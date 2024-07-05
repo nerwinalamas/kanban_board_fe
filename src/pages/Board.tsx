eeb7027b68e0e089eb8be48f66a8cff1cfd91f0a
nerwinalamas <nerwinalamas@gmail.com>
2024-07-05 13:58:01 +0800

    refactor: use column store and modal

--- src/pages/Board.tsx
+++ src/pages/Board.tsx
@@ -1,13 +1,36 @@
-import { useState } from "react";
-import { DndContext } from "@dnd-kit/core";
+import {
+    closestCorners,
+    DndContext,
+    DragEndEvent,
+    KeyboardSensor,
+    PointerSensor,
+    useSensor,
+    useSensors,
+} from "@dnd-kit/core";
 import DroppableColumn from "../components/DroppableColumn";
-import { columnsData } from "../data";
-import { SortableContext } from "@dnd-kit/sortable";
-import { Column } from "../types";
+import {
+    arrayMove,
+    SortableContext,
+    sortableKeyboardCoordinates,
+} from "@dnd-kit/sortable";
 import DraggableItem from "../components/DraggableItem";
+import useModal from "../hooks/useModal";
+import useColumnStore from "../store/useColumn";
 
 const Board = () => {
-    const [columns, setColumns] = useState<Column[]>(columnsData);
+    const { columns } = useColumnStore();
+    const { openModal } = useModal();
+
+    const sensors = useSensors(
+        useSensor(PointerSensor),
+        useSensor(KeyboardSensor, {
+            coordinateGetter: sortableKeyboardCoordinates,
+        })
+    );
+
+    const handleDragEnd = (e: DragEndEvent) => {
+        // const { active, over } = e;
+    };
 
     return (
         <div className="w-full grid p-4">
@@ -16,34 +39,52 @@ const Board = () => {
                     <h1 className="text-xl font-semibold text-white">
                         Kanban Board
                     </h1>
-                    <button className="py-2 px-4 rounded bg-slate-600 text-white hover:bg-slate-600/50">
+                    <button
+                        onClick={() => openModal("createColumnModal")}
+                        className="py-2 px-4 rounded bg-slate-600 text-white hover:bg-slate-600/50"
+                    >
                         Create
                     </button>
                 </div>
-                <div className="w-full flex gap-5 overflow-x-auto">
-                    <DndContext>
-                        <SortableContext items={columns}>
-                            {columns.map((column) => (
-                                <DroppableColumn
-                                    key={column.id}
-                                    id={column.id}
-                                    title={column.title}
-                                >
-                                    <SortableContext items={column.items}>
-                                        {column.items.map((item) => (
-                                            <DraggableItem
-                                                key={item.id}
-                                                id={item.id}
-                                                title={item.title}
-                                                description={item.description}
-                                            />
-                                        ))}
-                                    </SortableContext>
-                                </DroppableColumn>
-                            ))}
-                        </SortableContext>
-                    </DndContext>
-                </div>
+                {columns.length > 0 && (
+                    <div className="w-full flex gap-5 overflow-x-auto">
+                        <DndContext
+                            sensors={sensors}
+                            collisionDetection={closestCorners}
+                            onDragEnd={handleDragEnd}
+                        >
+                            <SortableContext
+                                items={columns.map((column) => column.id)}
+                            >
+                                {columns.map((task) => (
+                                    <DroppableColumn
+                                        key={task.id}
+                                        id={task.id}
+                                        title={task.title}
+                                    >
+                                        <SortableContext
+                                            items={task.items.map(
+                                                (item) => item.id
+                                            )}
+                                        >
+                                            {task.items.map((item) => (
+                                                <DraggableItem
+                                                    key={item.id}
+                                                    id={item.id}
+                                                    title={item.title}
+                                                    description={
+                                                        item.description
+                                                    }
+                                                    columnId={task.id}
+                                                />
+                                            ))}
+                                        </SortableContext>
+                                    </DroppableColumn>
+                                ))}
+                            </SortableContext>
+                        </DndContext>
+                    </div>
+                )}
             </div>
         </div>
     );
