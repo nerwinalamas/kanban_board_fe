5313e0f65987b6a8cfb4b7195c6b2b605e715f08
nerwinalamas <nerwinalamas@gmail.com>
2024-07-05 14:32:16 +0800

    refactor: item

--- src/components/DraggableItem.tsx
+++ src/components/DraggableItem.tsx
@@ -1,14 +1,19 @@
-import { useDraggable } from "@dnd-kit/core";
+// import { useDraggable } from "@dnd-kit/core";
 import { CSS } from "@dnd-kit/utilities";
 import { Item } from "../types";
+import { useSortable } from "@dnd-kit/sortable";
 
-const DraggableItem = ({ id, title, description }: Item) => {
-    const { attributes, listeners, setNodeRef, transform } = useDraggable({
+const DraggableItem = ({ id, title, description, columnId }: Item) => {
+    const { attributes, listeners, setNodeRef, transform, transition } = useSortable({
         id,
+        data: {
+            columnId,
+        },
     });
 
     const style = {
         transform: CSS.Translate.toString(transform),
+        transition,
     };
 
     return (
