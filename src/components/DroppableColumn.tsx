fbeafb43cdbb994a81ab2b53e8638ee9f11b63da
nerwinalamas <nerwinalamas@gmail.com>
2024-07-06 12:03:21 +0800

    feat: create new item

--- src/components/DroppableColumn.tsx
+++ src/components/DroppableColumn.tsx
@@ -8,14 +8,19 @@ import {
     DropdownMenuTrigger,
 } from "./ui/dropdown-menu";
 import useModal from "../hooks/useModal";
-import { useState } from "react";
+import React, { useState } from "react";
+import useColumnStore from "../store/useColumn";
+import { v4 as uuidv4 } from "uuid";
 
 const DroppableColumn = ({ id, title, children }: ColumnProps) => {
     const { setNodeRef } = useDroppable({
         id,
     });
     const { openModal } = useModal();
+    const { createItem } = useColumnStore();
     const [toggleDropDownMenu, setToggleDropDownMenu] = useState(false);
+    const [toggleAddItem, setToggleAddItem] = useState(false);
+    const [newItemTitle, setNewItemTitle] = useState("");
 
     const handleUpdateColumn = () => {
         openModal("updateColumnModal", {
@@ -34,10 +39,34 @@ const DroppableColumn = ({ id, title, children }: ColumnProps) => {
         setToggleDropDownMenu((prev) => !prev);
     };
 
+    const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+
+        try {
+            const newItem = {
+                id: uuidv4(),
+                title: newItemTitle,
+                description: "",
+                columnId: id,
+            };
+            createItem(id, newItem);
+        } catch (error) {
+            console.log("Create Item Error: ", error);
+        }
+
+        setNewItemTitle("");
+        setToggleAddItem(false);
+    };
+
+    const handleCancel = () => {
+        setToggleAddItem(false);
+        setNewItemTitle("");
+    };
+
     return (
         <div
             ref={setNodeRef}
-            className="w-72 bg-slate-700 flex flex-col flex-shrink-0 justify-between gap-5 p-4 rounded-lg"
+            className="w-72 h-max bg-slate-700 flex flex-col flex-shrink-0 justify-between gap-5 p-4 rounded-lg"
         >
             <div className="flex flex-col gap-5">
                 <div className="flex justify-between items-center">
@@ -70,15 +99,43 @@ const DroppableColumn = ({ id, title, children }: ColumnProps) => {
                         </DropdownMenuContent>
                     </DropdownMenu>
                 </div>
-                <div className="max-h-[480px] flex flex-col gap-2 overflow-y-auto">
+                <div className="max-h-[47vh] flex flex-col gap-2 overflow-y-auto">
                     {children}
                 </div>
             </div>
 
-            <div className="flex items-center gap-1 p-2 rounded-md cursor-pointer hover:bg-slate-600">
-                <Plus size={18} />
-                <p>Add card</p>
-            </div>
+            {toggleAddItem ? (
+                <form onSubmit={handleSubmit} className="flex flex-col gap-3">
+                    <input
+                        type="text"
+                        placeholder="Title"
+                        value={newItemTitle}
+                        onChange={(e) => setNewItemTitle(e.target.value)}
+                        className="p-4 rounded-lg cursor-pointer outline-none bg-slate-800"
+                    />
+                    <div className="flex gap-2">
+                        <input
+                            type="submit"
+                            value="Add"
+                            className="py-2 px-4 rounded cursor-pointer bg-slate-600 text-white hover:bg-slate-600/50"
+                        />
+                        <input
+                            type="button"
+                            value="Cancel"
+                            onClick={handleCancel}
+                            className="py-2 px-4 rounded cursor-pointer text-white hover:bg-slate-600/50"
+                        />
+                    </div>
+                </form>
+            ) : (
+                <div
+                    onClick={() => setToggleAddItem(true)}
+                    className="flex items-center gap-1 p-2 rounded-md cursor-pointer hover:bg-slate-600"
+                >
+                    <Plus size={18} />
+                    <p>Add card</p>
+                </div>
+            )}
         </div>
     );
 };
