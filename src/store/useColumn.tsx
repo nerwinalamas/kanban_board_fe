6f5209ffa1cc62feba4136ef4ed69bdd5d324760
nerwinalamas <nerwinalamas@gmail.com>
2024-07-06 12:02:53 +0800

    feat: item crud

--- src/store/useColumn.tsx
+++ src/store/useColumn.tsx
@@ -1,11 +1,23 @@
 import { create } from "zustand";
-import { Column } from "../types";
+import { Column, Item } from "../types";
 
 type ColumnState = {
     columns: Column[];
     createColumn: (data: Column) => void;
     updateColumn: (columnId: string, newTitle: string) => void;
     deleteColumn: (columnId: string) => void;
+    createItem: (columnId: string, item: Item) => void;
+    updateItem: (
+        columnId: string,
+        itemId: string,
+        updatedItem: Partial<Item>
+    ) => void;
+    deleteItem: (columnId: string, itemId: string) => void;
+    moveItem: (
+        sourceColumnId: string,
+        targetColumnId: string,
+        itemId: string
+    ) => void;
 };
 
 const useColumnStore = create<ColumnState>((set) => ({
@@ -24,6 +36,71 @@ const useColumnStore = create<ColumnState>((set) => ({
         set((state) => ({
             columns: state.columns.filter((column) => column.id !== columnId),
         })),
+    createItem: (columnId, item) =>
+        set((state) => ({
+            columns: state.columns.map((column) =>
+                column.id === columnId
+                    ? { ...column, items: [...column.items, item] }
+                    : column
+            ),
+        })),
+    updateItem: (columnId, itemId, updatedItem) =>
+        set((state) => ({
+            columns: state.columns.map((column) =>
+                column.id === columnId
+                    ? {
+                          ...column,
+                          items: column.items.map((item) =>
+                              item.id === itemId
+                                  ? { ...item, ...updatedItem }
+                                  : item
+                          ),
+                      }
+                    : column
+            ),
+        })),
+    deleteItem: (columnId, itemId) =>
+        set((state) => ({
+            columns: state.columns.map((column) =>
+                column.id === columnId
+                    ? {
+                          ...column,
+                          items: column.items.filter(
+                              (item) => item.id !== itemId
+                          ),
+                      }
+                    : column
+            ),
+        })),
+    moveItem: (sourceColumnId, targetColumnId, itemId) =>
+        set((state) => {
+            const itemToMove = state.columns
+                .find((column) => column.id === sourceColumnId)
+                ?.items.find((item) => item.id === itemId);
+            if (!itemToMove) return state;
+
+            return {
+                columns: state.columns.map((column) => {
+                    if (column.id === sourceColumnId) {
+                        return {
+                            ...column,
+                            items: column.items.filter(
+                                (item) => item.id !== itemId
+                            ),
+                        };
+                    } else if (column.id === targetColumnId) {
+                        return {
+                            ...column,
+                            items: [
+                                ...column.items,
+                                { ...itemToMove, columnId: targetColumnId },
+                            ],
+                        };
+                    }
+                    return column;
+                }),
+            };
+        }),
 }));
 
 export default useColumnStore;
